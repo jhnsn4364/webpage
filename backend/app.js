b6476e995c469a866dd608348d148403b680b0bb
Matthew W <matthew@willcockson.family>
2026-04-26 22:05:17 -0500

    feat(backend): basic express static files

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,15 @@
 // start your backend for the project with this file
+const express = require("express");
+const serveIndex = require("serve-index");
+const app = express();
+
+app.post("/form", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/", express.static("."), serveIndex(".", {}));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
