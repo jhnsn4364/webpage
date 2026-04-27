ade53594fa2e49a894f86940ca0a484f754334aa
Matthew W <matthew@willcockson.family>
2026-04-26 23:07:18 -0500

    fix(backend): nonexistent form parsing

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,40 @@
 // start your backend for the project with this file
+const path = require("path");
+
 const express = require("express");
 const serveIndex = require("serve-index");
-const app = express();
+const bodyParser = require("body-parser");
 
-app.post("/form", (req, res) => {
-  res.send("hello world");
+const app = express();
+const formParser = bodyParser.urlencoded({ inflate: false });
+
+const formUrl = "/form";
+const formErrorPage = "/form-error.html";
+
+const rooms = new Map();
+
+app.post(formUrl, formParser, (req, res, next) => {
+  const formData = req.body;
+  console.log("formData: %o", formData);
+
+  const requested_time = new Date(formData.booking_start);
+
+  const isValid = true;
+  if (!isValid) {
+    res.redirect(302, formErrorPage);
+    return;
+  }
+
+  rooms.set(
 });
 
-app.use("/", express.static("."), serveIndex(".", {}));
+// view
+app.get(formUrl, (req, res) => {
+
+});
+
+const webRoot = path.join(__dirname, "..", "frontend");
+app.use("/", express.static(webRoot), serveIndex(webRoot, {}));
 
 const port = 3000;
 app.listen(port, () => {
