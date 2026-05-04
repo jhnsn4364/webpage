120236eda005d4088076d8d516832e707e95c00b
Claire Johnson <claire.johnson4364@gmail.com>
2026-05-04 11:23:14 -0500

    Add files via upload
    Moving Matthew's backend implementation to its' own branch

--- README.md
+++ README.md
@@ -1,47 +1,6 @@
 # CIS2336 Spring 2026 Individual Project
-**Author: Claire Johnson**
 
-## Project Description
-The project goal was to create a new website for the UH Sugar Land Student Center. The final project deliverables include the HTML for 10 different webpages with CSS styling and multiple interactive Javascript elements. Additionally, the project includes the backend code to run a Node.js server to handle all relevant form data.
+Please update this README with the instructions on how to use the repository (developer documentation).
 
-## Project organization
-All the html, javascript, and css files necessary for hosting the website frontend are located in [:file_folder: frontend](frontend/).
+Use [GitHub Markdown](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github) to oraginze and fromat your README document.
 
-The javascript file containing the backend server is named ['app.js'](backend/app.js), and is located in [:file_folder: backend](backend/).
-
-All the locally hosted media elements used by the website are located in [:file_folder: media](media/).
-
-## Server requirements
-The server backend is designed to be run using Node.js
-
-- [Node.js installation instructions](https://nodejs.org/en/download)
-
-**Required Node.js Dependencies:**
-- Express
-- Cors
-
-## Quickstart
-1. Download and install [Node.js](https://nodejs.org/en/download)
-2. Download and install [git](https://git-scm.com/install/)
-3. Clone the git repository to your local machine
-```
-git clone https://github.com/CIS-UH/semester-project-ClaireJohnsonUH
-```
-4. Enter the repository directory
-```
-cd ./semester-project-ClaireJohnsonUH
-```
-5. Download the required Node.js dependencies using npm
-```
-npm install express
-npm install cors
-```
-6. Start the backend server
-```
-node ./backend/app.js
-```
-7. Use npx to host a basic local server
-```
-npx http-server -y
-```
-8. Visit http://127.0.0.1:8080/frontend/index.html in your webrowser
--- frontend/javascript.js
+++ frontend/javascript.js
@@ -1,7 +1,6 @@
 let navDictionary = {};
 navDictionary['Facilities']=`<ul class="nav-container">
                 <li><a href="map.html">Building Map</a></li>
-                <li><a href="dining.html">Dining Options</a></li>
                 <li><a href="reservations.html">Room Reservations</a></li>
                 </ul>`;
 navDictionary['Campus Services']=`<ul class="nav-container">
@@ -18,10 +17,13 @@ navDictionary['About Us']=`<ul class="nav-container">
 navDictionary['Developer Contact']=`<ul class="nav-container">
                 <li><a href="references.html">References</a></li>
                 </ul>`;
-navDictionary['Homepage']='';
+navDictionary['Homepage']=``;
 
 function topnav_hover(element){
     const submenu = document.getElementById('submenu');
+    if (element.innerText == 'Homepage'){
+        return
+    }
     submenu.innerHTML=navDictionary[element.innerText];
 
 }
@@ -34,17 +36,63 @@ document.addEventListener('DOMContentLoaded',()=>{
         submenu.innerHTML=``;
     })
 
-    let topnav = document.getElementsByClassName('nav-link');
+    let topnav = document.querySelectorAll('a');
     for (const element of topnav){
         element.addEventListener('mouseenter', function(){
             topnav_hover(element);
         })
+        
     }
 
-    let active = document.getElementsByClassName('nav-active');
-    for (const element of active){
-        element.addEventListener('mouseenter', function(){
-            topnav_hover(element);
-        })
-    }
+  if (url.pathname.endsWith("/reservations.html")) {
+    doForm()
+  }
 });
+
+const url = new URL(window.location.href);
+console.log("url: %o", url);
+const doForm = () => {
+  const validation_errors = JSON.parse(decodeURIComponent(url.hash.slice(1)));
+
+  const errorsDiv = document.createElement("div");
+  errorsDiv.classList.add("errors");
+  const errorsList = document.createElement("ul");
+  errorsDiv.insertAdjacentElement("beforeend", errorsList);
+
+  for (const key in validation_errors) {
+    const {original_value, error_messages} = validation_errors[key];
+    const formInput = document.getElementById(key);
+    if (formInput) {
+      switch (formInput.type) {
+        case "checkbox":
+          formInput.checked = original_value === "true";
+          break;
+        case "datetime-local":
+          if (original_value === "") {
+            continue;
+          }
+          const date = new Date(original_value);
+          const minutesOffsetFromUTC = date.getTimezoneOffset();
+          const secondsInMinute = 60;
+          const millisecondsInSecond = 1000;
+          formInput.valueAsNumber = date.getTime() - (minutesOffsetFromUTC * secondsInMinute * millisecondsInSecond);
+          break;
+        default:
+          formInput.value = original_value;
+          break;
+      }
+    } else {
+      console.error("missing id (%o) that matches validation object's: %o", key, validation_errors[key]);
+    }
+
+    for (const message of error_messages) {
+      const li = document.createElement("li");
+      li.textContent = message;
+      errorsList.insertAdjacentElement("beforeend", li);
+    }
+  }
+
+  if (errorsList.childElementCount > 0) {
+    document.querySelector("form").insertAdjacentElement("beforebegin", errorsDiv);
+  }
+};
